ff5d8d96d58d0e2786320c11983856ac8926894e
tyagidevansh <devanshtyagi0607@gmail.com>
2025-10-08 02:27:14 +0530

    added WASAPI audio capture

--- main.c
+++ main.c
@@ -1,125 +1,117 @@
 #include <stdio.h>
-#include "raylib.h"
-#include <unistd.h>
-#include "stdint.h"
-#include "string.h"
-#include <complex.h>
+#include <stdlib.h>
 #include <math.h>
+#include <complex.h>
 #include <assert.h>
-#if defined(PLATFORM_WEB)
-#include <emscripten/emscripten.h>
-#endif
-#define pi 3.14159265358979323846
 
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define Rectangle RB_Rectangle
+#define DrawText RB_DrawText
+#define DrawTextEx RB_DrawTextEx
+#define CloseWindow RB_CloseWindow
+#define ShowCursor RB_ShowCursor
+#define LoadImage RB_LoadImage
+#define PlaySound RB_PlaySound
+
+#include "raylib.h"
+
+#define rb_InitWindow InitWindow
+#define rb_SetTargetFPS SetTargetFPS
+#define rb_WindowShouldClose WindowShouldClose
+#define rb_BeginDrawing BeginDrawing
+#define rb_EndDrawing EndDrawing
+#define rb_ClearBackground ClearBackground
+#define rb_DrawRectangle DrawRectangle
+#define rb_GetRenderWidth GetRenderWidth
+#define rb_GetRenderHeight GetRenderHeight
+#define rb_CloseWindow RB_CloseWindow
+#define rb_Color Color
 
 #define N 256
+
 float in[N];
 float complex out[N];
 float max_amplitude;
 
-typedef struct {
-  float left_channel;
-  float right_channel;
-} Frame;
+void capture_system_audio(float *buffer, int samples);
 
-
-void fft(float in[],size_t stride, float complex out[],size_t step) {
+// literally the original function and it doesnt work the same
+void fft(float in[], size_t stride, float complex out[], size_t step)
+{
 
   assert(step > 0);
 
-  if(step == 1){
+  if (step == 1)
+  {
     out[0] = in[0];
     return;
   }
-  fft(in,stride,out,step/2);
-  fft(in+stride,stride,out+step/2,step/2);
-  for(size_t i = 0; i < step/2; i++){
-    float t = (float)i/step;
-    float complex v = cexp(-2*I*pi*t)*out[i+step/2];
+  fft(in, stride, out, step / 2);
+  fft(in + stride, stride, out + step / 2, step / 2);
+  for (size_t i = 0; i < step / 2; i++)
+  {
+    float t = (float)i / step;
+    float complex v = cexp(-2 * I * PI * t) * out[i + step / 2];
     float complex e = out[i];
     out[i] = e + v;
-    out[i+step/2] = e - v;
-   
+    out[i + step / 2] = e - v;
+  }
 }
 
-}
-
-
-
-
-float amp(float complex z){
+float amp(float complex z)
+{
   float a = fabsf(crealf(z));
   float b = fabsf(cimagf(z));
-  if(a < b) return b;
-  return a;
-
-
+  return a > b ? a : b;
 }
 
+int main(void)
+{
+  rb_InitWindow(800, 600, "Sickass Audio Visualizer");
+  rb_SetTargetFPS(60);
 
-void callback(void *bufferData, unsigned int frames) {
-  Frame *fs = (Frame *)bufferData;
-  size_t to_copy = frames < N ? frames : N;
+  while (!rb_WindowShouldClose())
+  {
+    capture_system_audio(in, N);
 
-  for(size_t i = 0; i < to_copy; i++){
-    in[i] = fs[i].left_channel;
-  }
-  for(size_t i = to_copy; i < N; i++){
-    in[i] = 0.0f;
-  }
+    fft(in, 1, out, N);
 
-  fft(in, 1, out, N);
-  max_amplitude = 0.0f;
-  for(size_t i = 0; i < N; i++){
-    float a = amp(out[i]);
-    if(a > max_amplitude) max_amplitude = a;
-  }
-}
+    max_amplitude = 0.0f;
+    for (int i = 0; i < N / 2; i++)
+    {
+      float a = amp(out[i]);
+      if (a > max_amplitude)
+        max_amplitude = a;
+    }
+    if (max_amplitude < 1.0f)
+      max_amplitude = 1.0f;
 
-int main(void) {
-  InitWindow(800, 600, "Hanuman");
-  SetWindowState(FLAG_WINDOW_RESIZABLE);
-  SetTargetFPS(60);
+    int width = rb_GetRenderWidth();
+    int height = rb_GetRenderHeight();
+    float cell_width = (float)width / (N / 2);
 
-  InitAudioDevice();
-  Music music = LoadMusicStream("hanuman.wav");
-  PlayMusicStream(music);
-  SetMusicVolume(music, 0.75f);
-  AttachAudioStreamProcessor(music.stream, callback);
+    rb_BeginDrawing();
+    rb_ClearBackground((rb_Color){0x18, 0x18, 0x18, 0xFF});
 
-  while (!WindowShouldClose()) {
-    UpdateMusicStream(music);
+    for (int i = 0; i < N / 2; i++)
+    {
+      float t = amp(out[i]) / max_amplitude;
+      if (t > 1.0f)
+        t = 1.0f;
 
+      int x = (int)(i * cell_width);
+      int y = (int)(height / 2 - (height / 2.0f * t));
+      int w = (int)ceilf(cell_width);
+      int h = (int)(height * t);
 
-    if (IsKeyPressed(KEY_SPACE)) {
-        if (IsMusicStreamPlaying(music)) {
-            PauseMusicStream(music);
-        }
-        else {
-            PlayMusicStream(music);
-        }
+      rb_Color red = {255, 0, 0, 255};
+      rb_DrawRectangle(x, y, w, h, red);
     }
 
-    int width = GetRenderWidth();
-    int height = GetRenderHeight();
-
-    BeginDrawing();
-    ClearBackground(CLITERAL(Color){0x18,0x18,0x18,0xFF});
-    //DrawText("Hanuman", width/2-strlen("Hanuman"), 10, 20, WHITE);
-    float cell_width = (float)width/(float)N;
-    for(size_t i = 0; i < N; i++){
-      float t = amp(out[i])
-      ;
-   
-      DrawRectangle((int)(i*cell_width), height/2-height/2*t, cell_width, height/2*t, RED);
-    }
-
-    EndDrawing();
+    rb_EndDrawing();
   }
 
+  // rb_CloseWindow();
   return 0;
 }
-
-
-
-
